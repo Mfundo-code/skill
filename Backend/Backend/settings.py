0354f7df452353717aab443cb91619cafcaa7274
Mfundo <mfundoknox@gmail.com>
2025-09-02 06:34:30 +0200

    django settings

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -111,9 +111,18 @@ SERVER_EMAIL = 'mfundoknox@gmail.com'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost', 
+    '127.0.0.1', 
+    "skill-hive.co.za", 
+    "www.skill-hive.co.za", 
+    "207.180.201.93"
+]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "skill-hive.co.za",
+    "www.skill-hive.co.za",
+    "207.180.201.93"
 ]
